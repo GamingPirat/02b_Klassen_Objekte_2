7cc44ab71fda901130435326df899bd8de037e12
Michael <GamingPirat@gmail.com>
2024-07-15 12:49:41 +0200

    02 / Felder 2. Iteration

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,12 +5,10 @@ public class Cat {
     // Eigenschaften / properties
     // Felder / fields / attributes
 
-    // pass by reference
-    String name = "Grizabella";
-    String furColor = "white";
-
-    // pass by value
-    int age = 29;
+    
+    public String name;// pass by reference
+    public String furColor;
+    public int age;    // pass by value
 
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
